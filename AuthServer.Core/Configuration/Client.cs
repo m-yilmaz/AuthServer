62e3127470b8c2f8a0d7982298cbef0b64ca54ed
Mustafa YILMAZ <meka.mustafayilmaz@outlook.com>
2022-07-30 12:12:20 +0300

    CoreLayer dtos & services

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+
+        // www.myapi.com, www.myapi2.com => dönen token bu 2 siteye erişebilir.
+        // audienceslar jwt'nin payloadında olacak
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticatorService.cs
@@ -0,0 +1,19 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticatorService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefleshToken(string refleshToken);
+        Task<Response<NoDataDto>> RevokeRefleshToken(string refleshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+
+    }
+}
