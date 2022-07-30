a886c794e64477c724fa3c40e8a81b5fcc0729a7
Mustafa YILMAZ <meka.mustafayilmaz@outlook.com>
2022-07-30 14:23:08 +0300

    Data Layer complated

---
+++ AuthServer.Data/Configurations/RefleshTokenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Configurations
+{
+    public class RefleshTokenConfiguration : IEntityTypeConfiguration<UserRefleshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefleshToken> builder)
+        {
+            builder.HasKey(x => x.UserId);
+            builder.Property(x => x.Code).IsRequired().HasMaxLength(200);
+        }
+    }
+}
