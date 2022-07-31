808c3696b6325241d4ebefe143e798b2decc12b7
Mustafa YILMAZ <meka.mustafayilmaz@outlook.com>
2022-07-31 21:29:28 +0300

    OptionsPattern & TokenService

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SharedLibrary.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace AuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // options pattern
+            services.Configure<CustomTokenOptions>(Configuration.GetSection("TokenOptions"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
