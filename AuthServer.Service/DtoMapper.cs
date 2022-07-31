046e61d3443980dc8bd7708aa0197259e5390114
Mustafa YILMAZ <meka.mustafayilmaz@outlook.com>
2022-07-31 11:49:29 +0300

    AutoMapper added

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    // Map yapmak için, profile classından miras aldık.
+    internal class DtoMapper : Profile
+    {
+        // map lenecek entityler ctor a eklenir.
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
